0d2e9ae4d4073b333e0feb5e49b0b2ce128affcf
Romayssae El Fihi <relfihi@gmail.com>
2026-05-28 13:35:35 +0100

    Premier commit

---
+++ src/main/java/org/example/intellipm/IntellipmApplication.java
@@ -0,0 +1,13 @@
+package org.example.intellipm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellipmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntellipmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/intellipm/IntellipmApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.intellipm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntellipmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
