41d7b4367d5c04d3bb4afdce82a9424120e0047f
Romayssae El Fihi <relfihi@gmail.com>
2026-06-21 00:08:11 +0100

    Fix: correction affichage chef de projet

--- src/main/java/com/example/intellipm/security/SecurityConfig.java
+++ src/main/java/com/example/intellipm/security/SecurityConfig.java
@@ -55,6 +55,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/auth/**")
                         .permitAll()
 
+
                         // Pages HTML
                         .requestMatchers(
                                 "/",
